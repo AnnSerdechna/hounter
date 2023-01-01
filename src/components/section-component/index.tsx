eee93a4e696b24e835f567b9f9e0ba47e4a1fcdc
Anna Serdechna <anna.serdechna@bndigital.co>
2023-01-01 14:45:44 +0200

    Add bottom cards to hero

--- src/components/section-component/index.tsx
+++ src/components/section-component/index.tsx
@@ -3,11 +3,11 @@ import { Col } from 'antd'
 
 import './index.less'
 
-const SectionComponent: FC<PropsWithChildren<{ maxWidth?: string | number, paddingBottom?: string | number, paddingTop?: string | number }>> = ({ children, maxWidth = 1200, paddingBottom = 120, paddingTop = 120 }) => (
+const SectionComponent: FC<PropsWithChildren<{ maxWidth?: string | number, paddingBottom?: string | number }>> = ({ children, maxWidth = 1200, paddingBottom = 120}) => (
   <Col
     span={24}
     className={'section'}
-    style={{ maxWidth: maxWidth, paddingBottom: paddingBottom, paddingTop: paddingTop }}
+    style={{ maxWidth: maxWidth, paddingBottom: paddingBottom }}
   >
     {children}
   </Col>
--- src/sections/hero/index.tsx
+++ src/sections/hero/index.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react'
-import {Row, Col, Typography, Space} from 'antd'
+import {Row, Col, Typography, Space, Card, Carousel, Avatar} from 'antd'
 
 
 import {Paragraph, SectionComponent, SvgIcon} from '../../components'
@@ -13,29 +13,39 @@ const { Title } = Typography
 
 console.log(usaStates.length)
 
+const settings = {
+  infinite: true,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  draggable: true,
+  dots: true,
+};
+
 
 const Hero: FC = () => (
-  <Row className={'hero'}>
-    <img src={HeroBg} alt={'Hero'} className={'hero-bg'} />
+  <>
+    <Row className={'hero'}>
+      <img src={HeroBg} alt={'Hero'} className={'hero-bg'} />
 
-    <SectionComponent paddingBottom={0} paddingTop={0}>
-      <Col span={12}>
-        <Space direction={'vertical'} size={24} style={{marginBottom: 16}}>
-          <Col span={16}>
-            <Title className={'hero-title'}>
-              find the place to live{' '}
-              <span className={'hero-title outlined'}>your dreams</span>{' '}easily here
-            </Title>
-          </Col>
+      <SectionComponent>
+        <Col span={12}>
+          <Space direction={'vertical'} size={24} style={{marginBottom: 16}}>
+            <Col span={16}>
+              <Title className={'hero-title'}>
+                find the place to live{' '}
+                <span className={'hero-title outlined'}>your dreams</span>{' '}easily here
+              </Title>
+            </Col>
 
-          <Col span={20}>
-            <Paragraph
-              text={'Everything you need about finding your place to live will be here, where it will be easier for you'}
-              fontSize={16}
-              style={{marginBottom: 0}}
-            />
-          </Col>
-        </Space>
+            <Col span={20}>
+              <Paragraph
+                text={'Everything you need about finding your place to live will be here, where it will be easier for you'}
+                fontSize={16}
+                style={{marginBottom: 0}}
+              />
+            </Col>
+          </Space>
 
 
           <Col span={20} style={{marginBottom: 32}}>
@@ -45,10 +55,41 @@ const Hero: FC = () => (
           <Col>
             <Partnership />
           </Col>
+        </Col>
+      </SectionComponent>
 
-      </Col>
-    </SectionComponent>
-  </Row>
+      <Row style={{background: 'teal', width: '100%', marginTop: -100, zIndex: 200}} justify={'end'}>
+        <Col span={11}>
+          <Carousel {...settings}>
+            <Col  span={22} offset={2}>
+              <Card title={'Card title 1'} />
+            </Col>
+            <Col span={22} offset={2}>
+              <Card title={'Card title 2'} />
+            </Col>
+            <Col  span={22} offset={2}>
+              <Card title={'Card title 3'} />
+            </Col>
+            <Col  span={22} offset={2}>
+              <Card>
+                <Card.Meta
+                  title={'1K+ People'}
+                  description={'Successfully Getting Home'}
+                  avatar={(
+                    <Avatar.Group>
+                      <Avatar src={'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80'} />
+                      <Avatar src={'https://images.pexels.com/photos/2467401/pexels-photo-2467401.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'} />
+                      <Avatar src={'https://www.givedirectly.org/wp-content/uploads/2022/11/Adela-in-Rwanda-705x470.png'} />
+                    </Avatar.Group>
+                  )}
+                />
+              </Card>
+            </Col>
+          </Carousel>
+        </Col>
+      </Row>
+    </Row>
+  </>
 )
 
 export { Hero }
