b77057a12e942c09a53530bc0bd4aa3d877086e0
Anna Serdechna <anna.serdechna@bndigital.co>
2023-01-01 19:53:18 +0200

    Fixed hero

--- src/sections/hero/index.tsx
+++ src/sections/hero/index.tsx
@@ -18,15 +18,14 @@ const Hero: FC = () => (
       <img src={HeroBg} alt={'Hero'} className={'hero-bg'} />
 
       <SectionComponent>
-        <Col span={12}>
-          <Space direction={'vertical'} size={24} style={{marginBottom: 16}}>
+        <Col span={12} className={'hero-content'}>
+          <Space direction={'vertical'} size={24} style={{marginBottom: 20}}>
             <Col span={16}>
               <Title className={'hero-title'}>
                 find the place to live{' '}
                 <span className={'hero-title outlined'}>your dreams</span>{' '}easily here
               </Title>
             </Col>
-
             <Col span={20}>
               <Paragraph
                 text={'Everything you need about finding your place to live will be here, where it will be easier for you'}
@@ -35,8 +34,6 @@ const Hero: FC = () => (
               />
             </Col>
           </Space>
-
-
           <Col span={20} style={{marginBottom: 32}}>
             <SearchSelect />
           </Col>
